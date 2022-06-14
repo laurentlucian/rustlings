3ab9589976c4910b1e728183cda53418f177b4d8
Laurent <laurent.lucian@gmail.com>
2022-06-13 17:36:03 -0700

    generics

--- exercises/generics/generics1.rs
+++ exercises/generics/generics1.rs
@@ -4,6 +4,6 @@
 // Execute `rustlings hint generics1` for hints!
 
 fn main() {
-    let mut shopping_list: Vec<String> = Vec::new();
+    let mut shopping_list: Vec<&str> = Vec::new();
     shopping_list.push("milk");
 }
